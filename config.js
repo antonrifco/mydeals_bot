28456a4b4c3638e81eca0f647ae35e430aa06ff7
Anton <anton.rifco@gmail.com>
2020-06-12 13:56:49 +0700

    fix issue

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    'db_name': 'mobejo',
-    'db_path': "mongodb+srv://bejo:mrBej0K3jo&B4rj0@cluster0-9sfz3.mongodb.net/mobejo?retryWrites=true",
+    'db_name': '',
+    'db_path': "",
     'page_size': 9,
     'small_page_size': 4
 };
